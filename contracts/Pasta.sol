cd7df606a97b37ba98c7a40849325261ad2c74a0
Bruno Castrillon <brunocastrillon@gmail.com>
2021-08-22 16:22:27 -0400

    gerenciador-arquivo-wip

---
+++ contracts/Pasta.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Pasta {
+    struct Arquivo {
+        string Hash;
+        string Nome;
+        string Tipo;
+        uint DataEnvio;
+    }    
+
+    mapping(address => Arquivo[]) _arquivos;
+
+    event Salvo(string hashh, address remetente);
+
+    constructor() {
+
+	}    
+
+    function adicionar(
+        string memory hashh,
+        string memory nome,
+        string memory tipo,
+        uint data
+    )
+        public
+    {
+        _arquivos[msg.sender].push(Arquivo({
+            Hash: hashh,
+            Nome: nome,
+            Tipo: tipo,
+            DataEnvio: data
+        }));
+
+        emit Salvo(hashh, msg.sender);
+    }
+
+    function listar()
+        public
+        view
+        returns (uint)
+    {
+        return _arquivos[msg.sender].length;
+    }
+
+    function ler(uint index)
+        public
+        view
+        returns
+        (
+            string memory hashh,
+            string memory nome,
+            string memory tipoh,
+            uint data
+        )
+    {
+        Arquivo memory arquivo = _arquivos[msg.sender][index];
+        return (
+            arquivo.Hash,
+            arquivo.Nome,
+            arquivo.Tipo,
+            arquivo.DataEnvio
+        );
+    }
+}

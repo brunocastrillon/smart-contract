1c8e5f9961b14907c178a06cdd12d0c2fce2e9b3
Bruno Castrillon <brunocastrillon@gmail.com>
2021-10-20 17:39:08 -0400

    refatoração-wip
    - incluído a função 'sendPayment', a função esta registrando o pagamento apenas, e enviando fundos para o contrato

--- test/Store.test.ts
+++ test/Store.test.ts
@@ -19,15 +19,14 @@ use(solidity);
 
 describe('manage-store', () => {
     let _store: {
-        sendOrder: (itemMenu: string, quantity: number, dateTime: number, sender: { from: string; }) => any;
-        checkOrder: (id: number, sender: { from: string; }) => any;
-        total: (sender: { from: string; }) => any;
-        // enviarPreco:(arg0: string, arg1: string, arg2: { from: string; }) => any;
-        // enviarPagamentoSeguro: (arg0: number, arg1: { from: string; }) => any;
+        sendOrder: (itemMenu: string, quantity: number, dateTime: number, sender: { from: string }) => any;
+        checkOrder: (index: number, sender: { from: string }) => any;
+        sendPrice: (index: number, price: number, customer: string, sender: { from: string }) => any;
+        total: (sender: { from: string }) => any;
+        sendPayment: (index: number, paymentDate: number, sender: { from: string, value: number }) => any;
         // enviarFatura: (arg0: string, arg1: string, arg2: { from: string; }) => any;
         // obterFatura:(arg0: number, arg1: { from: string; }) => any;
         // MarcarPedidoEntregue:(arg0: string, arg1: string, arg2: { from: string; }) => any;
-        // _dono:() => any;
     };
 
     const _itemMenuNULL = "";
@@ -38,9 +37,10 @@ describe('manage-store', () => {
     const _orderIndex = 0;
     const _invoiceIndex = 0;
     const _itemMenu = "express-mj";
-    const _price = 69;
+    const _price = 6;
     const _quantity = 3;
-    const _payment = 207;//_price * _quantity;
+    const _payment = 18; //_price * _quantity;
+    const _paymentDate = (new Date()).getTime();
     const _orderDate = (new Date()).getTime();
     const _deliveryDate = (new Date()).getTime() + 30000;
 
@@ -59,7 +59,7 @@ describe('manage-store', () => {
             const store = await _store.sendOrder(_itemMenu, _quantity, _orderDate, { from: _customer });
 
             truffleAssert.eventEmitted(store, 'OrderDispatched', (ev: { itemMenu: string; quantity: number; orderDate: number, orderNumber: number, customer: string }) => {
-                return ev.itemMenu === _itemMenu && ev.quantity == _quantity && ev.orderDate == _orderDate && ev.orderNumber == _orderIndex+1 && ev.customer == _customer;
+                return ev.itemMenu === _itemMenu && ev.quantity == _quantity && ev.orderDate == _orderDate && ev.orderNumber == _orderIndex + 1 && ev.customer == _customer;
             });
         });
     });
@@ -76,10 +76,40 @@ describe('manage-store', () => {
         });
     });
 
+    describe('Testando a função sendPrice()', () => {
+        it('Usuário que não é dono do contrato, tentou enviar o preço, deverá retornar - Ownable: caller is not the owner', async () => {
+            await _store.sendOrder(_itemMenu, _quantity, _orderDate, { from: _customer });
+            await expect(
+                _store.sendPrice(_orderIndex, _price, _customer, { from: _customer })
+            ).to.be.revertedWith("Ownable: caller is not the owner");
+        });
+
+        it('Ao enviar o preço com sucesso, deverá retornar um evento: PriceSubmitted()', async () => {
+            await _store.sendOrder(_itemMenu, _quantity, _orderDate, { from: _customer });
+            const store = await _store.sendPrice(_orderIndex, _price, _customer, { from: _owner });
+
+            truffleAssert.eventEmitted(store, 'PriceSubmitted', (ev: { orderNumber: number, orderPrice: number }) => {
+                return ev.orderNumber == _orderIndex && ev.orderPrice == _price;
+            });
+        });
+    });
+
+    describe('Testando a função sendPayment', () => {
+        it('Ao realizar um pagamento com suceso, deverá retornar um event: PaymentSent()', async () => {
+            await _store.sendOrder(_itemMenu, _quantity, _orderDate, { from: _customer });
+            await _store.sendPrice(_orderIndex, _price, _customer, { from: _owner });
+            const store = await _store.sendPayment(_orderIndex, _paymentDate, { from: _customer, value: _payment });
+
+            truffleAssert.eventEmitted(store, 'PaymentSent', (ev: { orderNumber: number, payment: number, paymentDate: number }) => {
+                return ev.orderNumber == _orderIndex && ev.payment == _payment && ev.paymentDate == _paymentDate;
+            });            
+        });
+    });
+
     it("Deverá retornar o total de pedidos", async () => {
         await _store.sendOrder(_itemMenu, _quantity, _orderDate, { from: _customer });
         let pedidos = await _store.total({ from: _customer });
-        
+
         assert.equal(pedidos, 1);
-    });  
+    });
 });
